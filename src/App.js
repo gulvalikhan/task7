e4254b1c4f3bd16094ea0581fc88e5675f8d7f10
gulvalikhan <gulbarshyn.valikhan@narxoz.kz>
2024-04-03 15:09:11 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import ImageGallery from './ImageGallery';
+import LazyImage from './LazyImage';
+import CriticalImagePreloader from './CriticalImagePreloader';
+
+function App() {
+  return (
+      <div className="App">
+        <h1> Gallery </h1>
+        <CriticalImagePreloader src="link_to_critical_image.jpg" />
+        <LazyImage />
+        <ImageGallery />
+      </div>
+  );
+}
+
+export default App;
